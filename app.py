d2dc074fa5ae5fa7a55710452e63bf3c11307fdb
WTaichi <121156546+WTaichi@users.noreply.github.com>
2023-01-19 13:41:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+#Streamlitのインストール
 import streamlit as st
 
-# list of questions and answers
+# 問題文とその選択肢をリスト表記する
 questions = [
     ("What type of food is sushi?", ["Japanese", "Mexican", "Italian"]),
     ("What is the main ingredient in a Caesar salad?", ["Romaine lettuce", "Spinach", "Kale"]),
@@ -9,23 +10,23 @@ questions = [
     ("Which type of cuisine is famous for its dumplings?", ["Chinese", "Italian", "Mexican"]),
 ]
 
-# initialize score
+# 正解数を示す変数scoreを定義し0を代入
 score = 0
 
-# loop through questions
+# クイズをループ形式で表現する
 for question, options in questions:
-    # display question
+    # 問題文の表記
     st.write(question)
 
-    # get user's answer
+    # ユーザーに選択肢を選ばせるセレクトボックスを作成
     answer = st.selectbox("Choose the correct answer:", options)
 
-    # check if answer is correct
+    # 正誤判定
     if answer == options[0]:
         score += 1
         st.write("Correct!")
     else:
         st.write("Incorrect!")
 
-# display final score
+# 最終的なスコアを表示する
 st.write("Your final score is:", score)
